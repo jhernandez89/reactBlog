7c8f02b5163255cd350de208df7de1574982df36
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-10-13 10:08:44 -0500

    started get request

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+const lame = require('../lame.js')
+
+const API_KEY = lame.APIkey
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
+
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+}
