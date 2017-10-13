33fd9f2beea638751d5596b1a0bf5f4f64b6f4fe
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-10-13 11:32:26 -0500

    get request works

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,8 +5,10 @@ const API_KEY = lame.APIkey
 export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+console.log('hlwjeflkjwef');
 
 export function fetchPosts() {
+  console.log('helloojoj');
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
 
   return {
