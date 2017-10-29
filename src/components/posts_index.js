90a0be5784f4a5821cc16acffb35c6809c352670
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-10-29 15:32:09 -0500

    added fields to post new

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this
+      .props
+      .fetchPosts();
   }
 
   renderPosts() {
-    console.log(this.props.posts,'logging');
+    console.log(this.props.posts, 'logging');
     return _.map(this.props.posts, post => {
       return (
         <li className="list-group-item" key={post.id}>
@@ -22,6 +25,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
@@ -32,7 +40,7 @@ class PostsIndex extends Component {
 }
 
 function mapStateToProps(state) {
-  return { posts: state.posts }
+  return {posts: state.posts}
 }
 
-export default connect(mapStateToProps, { fetchPosts }) (PostsIndex);
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input 
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
   render() {
     return (
-      <div>
-        Postsnew!
-      </div>
-    )
+      <form>
+        <Field 
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field 
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    );
   }
 }
-export default PostsNew
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a title!';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content please';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew)
