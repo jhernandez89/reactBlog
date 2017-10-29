8a3831f25b9bb302b968807058714f7d8a65c76e
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-10-29 16:35:11 -0500

    validation added to new post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,20 +3,30 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: {touched, error } } = field
+    const className=`form-group ${touched && error ? 'has-danger': ''}`
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input 
           className="form-control"
           type="text"
           {...field.input}
         />
+        <div className="text-help has-danger">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
+  onSubmit(values) {
+    console.log(values)
+  }
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field 
           label="Title"
           name="title"
@@ -32,6 +42,7 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
